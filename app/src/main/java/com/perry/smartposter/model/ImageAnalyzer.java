9b93bc36c791716ba8203f763d5c626e896d738f
AMagicPear <AMagicPear@outlook.com>
2025-05-25 15:48:16 +0800

    feat: 实现定位点

--- app/src/main/java/com/perry/smartposter/model/ImageAnalyzer.java
+++ app/src/main/java/com/perry/smartposter/model/ImageAnalyzer.java
@@ -2,8 +2,12 @@ package com.perry.smartposter.model;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ImageFormat;
+import android.graphics.Paint;
 import android.graphics.PointF;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.Image;
 import android.os.Environment;
 import android.util.Log;
@@ -24,6 +28,7 @@ import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.face.Face;
 import com.perry.smartposter.R;
 import com.perry.smartposter.activity.MainActivity;
+import com.perry.smartposter.util.FacePainter;
 import com.perry.smartposter.util.ImageStorage;
 
 import java.nio.ByteBuffer;
@@ -32,6 +37,7 @@ import java.util.List;
 public class ImageAnalyzer implements ImageAnalysis.Analyzer {
     private final MainActivity activity;
     private BottomSheetDialog bottomSheetDialog;
+    private View bottomSheetView;
     /// 用于缓存过程中的bitmap
     private Bitmap cachedBitmap;
 
@@ -78,14 +84,27 @@ public class ImageAnalyzer implements ImageAnalysis.Analyzer {
     /// 在底部弹出一个半屏，展示发型选项
     private void showBottomSheet(List<PointF> faceContourPoints) {
         bottomSheetDialog = new BottomSheetDialog(activity);
-        View bottomSheetView = LayoutInflater.from(activity).inflate(R.layout.bottom_sheet_layout, activity.findViewById(android.R.id.content), false);
+        bottomSheetView = LayoutInflater.from(activity).inflate(R.layout.bottom_sheet_layout, activity.findViewById(android.R.id.content), false);
         ImageView imageView = bottomSheetView.findViewById(R.id.bottom_sheet_img);
         Log.d("Perry", "Bitmap is null? " + (cachedBitmap == null));
         Log.d("Perry", "Face contour points size: " + faceContourPoints.size());
         imageView.setImageBitmap(cachedBitmap);
+        setupButtons(bottomSheetView);
+        try {
+            FacePainter.drawHaircut(activity, bottomSheetView.findViewById(R.id.bottom_sheet_overlay), faceContourPoints);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        bottomSheetDialog.setContentView(bottomSheetView);
+        bottomSheetDialog.show();
+    }
+
+
+    private void setupButtons(View bottomSheetView) {
         Button yesButton = bottomSheetView.findViewById(R.id.yes_button);
         Button noButton = bottomSheetView.findViewById(R.id.no_button);
         TextInputEditText nameEditText = bottomSheetView.findViewById(R.id.text_input);
+
         yesButton.setOnClickListener(v -> {
             var text = nameEditText.getText();
             if (text == null || text.toString().isEmpty()) {
@@ -97,12 +116,11 @@ public class ImageAnalyzer implements ImageAnalysis.Analyzer {
             Toast.makeText(activity, "保存成功", Toast.LENGTH_SHORT).show();
             bottomSheetDialog.dismiss();
         });
+
         noButton.setOnClickListener(v -> {
             Log.d("Perry", "showBottomSheet: nobutton");
             bottomSheetDialog.dismiss();
         });
-        bottomSheetDialog.setContentView(bottomSheetView);
-        bottomSheetDialog.show();
     }
 
     /// 将 Image 转换成 Bitmap
--- app/src/main/java/com/perry/smartposter/util/FacePainter.java
+++ app/src/main/java/com/perry/smartposter/util/FacePainter.java
@@ -1,43 +1,40 @@
 package com.perry.smartposter.util;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
-import android.widget.ImageView;
+import android.graphics.drawable.BitmapDrawable;
+import android.view.View;
 
 import java.util.List;
 
 public final class FacePainter {
-    public static Bitmap drawFaceContours(Bitmap capturedImageBitmap, List<PointF> faceContourPoints) {
-        if (capturedImageBitmap != null && !capturedImageBitmap.isRecycled() && faceContourPoints != null && !faceContourPoints.isEmpty()) {
-            // 根据原始bitmap图创建可修改副本
-            Bitmap mutableBitmap = capturedImageBitmap.copy(Bitmap.Config.ARGB_8888, true);
-            Canvas canvas = new Canvas(mutableBitmap);
+    public static void drawHaircut(Activity activity, View overlayView, List<PointF> faceContourPoints) {
+        Bitmap overlayBitmap = Bitmap.createBitmap(244, 326, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(overlayBitmap);
 
-            // 创建Paint实例用于画线
-            Paint paint = new Paint();
-            paint.setColor(Color.RED);
-            paint.setStrokeWidth(5f);
-            paint.setStyle(Paint.Style.STROKE);
-
-            // 遍历脸轮廓数组，依次连线
-            for (int i = 0; i < faceContourPoints.size() - 1; i++) {
-                PointF startPoint = faceContourPoints.get(i);
-                PointF endPoint = faceContourPoints.get(i + 1);
-                canvas.drawLine(startPoint.x, startPoint.y, endPoint.x, endPoint.y, paint);
-            }
-
-            // 连接轮廓的首尾两点
-            if (faceContourPoints.size() > 1) {
-                PointF firstPoint = faceContourPoints.get(0);
-                PointF lastPoint = faceContourPoints.get(faceContourPoints.size() - 1);
-                canvas.drawLine(lastPoint.x, lastPoint.y, firstPoint.x, firstPoint.y, paint);
-            }
-
-            return mutableBitmap;
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(5f);
+        paint.setStyle(Paint.Style.STROKE);
+        // 遍历脸轮廓数组，依次连线
+        for (int i = 0; i < faceContourPoints.size() - 1; i++) {
+            PointF startPoint = faceContourPoints.get(i);
+            PointF endPoint = faceContourPoints.get(i + 1);
+            canvas.drawLine(startPoint.x / 10, startPoint.y / 10, endPoint.x / 10, endPoint.y / 10, paint);
         }
-        return null;
+
+        // 连接轮廓的首尾两点
+        if (faceContourPoints.size() > 1) {
+            PointF firstPoint = faceContourPoints.get(0);
+            PointF lastPoint = faceContourPoints.get(faceContourPoints.size() - 1);
+            canvas.drawLine(lastPoint.x / 10, lastPoint.y / 10, firstPoint.x / 10, firstPoint.y / 10, paint);
+        }
+
+        // 设置View背景
+        overlayView.setBackground(new BitmapDrawable(activity.getResources(), overlayBitmap));
     }
 }
